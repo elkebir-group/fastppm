4a4d29ea492fee747e9e07a4f08eca2b587bdcb7
Mohammed El-Kebir <melkebir@illinois.edu>
2025-12-04 11:27:01 -0600

    Updated build logic to integrate version number

--- build.sh
+++ build.sh
@@ -3,10 +3,18 @@
 mkdir -p build
 cd build
 
-cmake -DPYTHON_EXECUTABLE=$PYTHON -DCMAKE_BUILD_TYPE=RELEASE ..
+# Pass the Conda version variable ($PKG_VERSION) to CMake
+cmake -DPYTHON_EXECUTABLE=$PYTHON \
+      -DCMAKE_BUILD_TYPE=RELEASE \
+      -DFASTPPM_VERSION=$PKG_VERSION \
+      ..
 
 make -j${CPU_COUNT}
 
+# Make sure target directories exist before copying
+mkdir -p $PREFIX/bin
+mkdir -p $SP_DIR
+
+# Install the binary and the python module
 cp src/fastppm-cli $PREFIX/bin
 cp src/fastppm*.so $SP_DIR
-
